8700e688042ed3777f35d7abfc47b96120ecdb46
Andrey Eremenko <fr0ntender@yandex.ru>
2016-05-02 11:02:59 +0300

    Add main-menu block

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,17 @@ gulp.task('less', function () {
 });
 
 gulp.task('template', function () {
-	gulp.src('./public_html/develop/templates/index.html')
+	gulp.src('./public_html/develop/index.html')
 		.pipe(rigger())
 		.pipe(gulp.dest('./public_html'));
 });
 
 gulp.task('default', function () {
-    gulp.watch(['./public_html/develop/templates/*.html'], ['template']);
-    gulp.watch(['./public_html/develop/less/*.less'], ['less']);
+    gulp.watch([
+        './public_html/develop/templates/*.html',
+        './public_html/develop/blocks/*/*.html'], ['template']);
+    gulp.watch([
+        './public_html/develop/less/*.less',
+        './public_html/develop/blocks/*/*.less'], ['less']);
 });
 
