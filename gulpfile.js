63e5cc92f719a0ecd4bb770adcd2841a1aeb78b7
Andrey Eremenko <fr0ntender@yandex.ru>
2016-05-02 18:04:10 +0300

    Changed structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,23 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer');
     
 gulp.task('less', function () {
-  return gulp.src('./public_html/develop/less/template.less')
+  return gulp.src('./develop/less/template.less')
     .pipe(less())
-//   .pipe(sourcemaps.init())
     .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
-//   .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./public_html/static/css'));
 });
 
 gulp.task('template', function () {
-    return gulp.src('./public_html/develop/index.html')
+    return gulp.src('./develop/index.html')
             .pipe(rigger())
             .pipe(gulp.dest('./public_html'));
 });
 
 gulp.task('default', function () {
     gulp.watch([
-        './public_html/develop/*.html'], ['template']);
+        './develop/**/*.html'], ['template']);
     gulp.watch([
-        './public_html/develop/less/*.less',
-        './public_html/develop/blocks/*/*.less'], ['less']);
+        './develop/less/*.less',
+        './develop/blocks/*/*.less'], ['less']);
 });
 
