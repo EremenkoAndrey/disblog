cbdb53ab24385ec218c25fdf839b1a48e6bf7627
Andrey Eremenko <fr0ntender@yandex.ru>
2016-05-02 14:24:33 +0300

    Added PostCss and Page-titile block

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    rigger = require('gulp-rigger');
+    rigger = require('gulp-rigger'),
+    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer');
     
 gulp.task('less', function () {
   return gulp.src('./public_html/develop/less/template.less')
     .pipe(less())
+//   .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
+//   .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./public_html/static/css'));
 });
 
 gulp.task('template', function () {
-	gulp.src('./public_html/develop/index.html')
-		.pipe(rigger())
-		.pipe(gulp.dest('./public_html'));
+    return gulp.src('./public_html/develop/index.html')
+            .pipe(rigger())
+            .pipe(gulp.dest('./public_html'));
 });
 
 gulp.task('default', function () {
     gulp.watch([
-        './public_html/develop/templates/*.html',
-        './public_html/develop/blocks/*/*.html'], ['template']);
+        './public_html/develop/*.html'], ['template']);
     gulp.watch([
         './public_html/develop/less/*.less',
         './public_html/develop/blocks/*/*.less'], ['less']);
